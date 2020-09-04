a2caec31c28800fa5d603bd0bb281c49e8669ff5
Aaron Powell <me@aaron-powell.com>
2020-09-04 14:57:31 +1000

    little tweak for showing different state

--- example/src/MainApp.tsx
+++ example/src/MainApp.tsx
@@ -2,22 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 
 const MainApp = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>React Foldable</h1>
+      <p>
+        This is a component that makes it easier to detect foldable UI's and
+        have your React application adapt for them.
+      </p>
+      <p className="non-foldable">Looks like this isn't a foldable device.</p>
+    </header>
+  </div>
+);
 
-  export { MainApp }
+export { MainApp };
