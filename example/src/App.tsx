cea44e74c9b7dfe67eeb9c8aaf676178336bac32
Aaron Powell <me@aaron-powell.com>
2020-09-04 14:50:39 +1000

    makign a better demo app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,36 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Foldable, FoldableScreen } from "react-foldable";
+import { MainApp } from "./MainApp";
+import { SecondScreen } from "./SecondScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Foldable>
-          <FoldableScreen
-            matchScreen={0}
-            component={() => <>I'm the first screen</>}
-          ></FoldableScreen>
-          <FoldableScreen
-            matchScreen={1}
-            component={() => <>I'm the second screen</>}
-          ></FoldableScreen>
-        </Foldable>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Foldable>
+      <FoldableScreen matchScreen={0} component={MainApp} />
+      <FoldableScreen matchScreen={1} component={SecondScreen} />
+    </Foldable>
   );
 }
 
---
+++ example/src/SecondScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SecondScreen = () => (
+  <div className="second">
+    <p>You only see me if you're lucky to have a foldable</p>
+  </div>
+);
+
+export { SecondScreen };
