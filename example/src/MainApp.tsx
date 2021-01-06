fe469f2e5a07698816c88d6593bdabea008d4107
Aaron Powell <me@aaron-powell.com>
2021-01-06 04:49:56 +0000

    Adding to the demo

--- example/src/MainApp.tsx
+++ example/src/MainApp.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Foldable, FoldableScreen } from "@aaronpowell/react-foldable";
+import {
+  Foldable,
+  FoldableScreen,
+  useIsPotentiallyDualScreenDevice,
+} from "@aaronpowell/react-foldable";
 
 const MainApp = () => (
   <div className="App">
@@ -11,7 +15,16 @@ const MainApp = () => (
         This is a component that makes it easier to detect foldable UI's and
         have your React application adapt for them.
       </p>
-      <p className="non-foldable">Looks like this isn't a foldable device.</p>
+      <div className="non-foldable">
+        <p>Looks like this isn't a spanned foldable device.</p>
+        {useIsPotentiallyDualScreenDevice() && (
+          <p>
+            User Agent sniffing says you might be on a foldable, try spanning
+            multiple screens.
+          </p>
+        )}
+      </div>
+
       <Foldable>
         <FoldableScreen
           match={({ isDualScreen }) => isDualScreen}
